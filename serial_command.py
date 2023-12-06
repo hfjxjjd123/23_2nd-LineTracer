4afa1578c706869614c60bd389de7c5d125167fd
hfjxjjd123 <licbusiness21@gmail.com>
2023-12-06 19:19:15 +0900

    port init

--- serial_command.py
+++ serial_command.py
@@ -2,7 +2,7 @@ import serial
 
 def cmd_arduino(left, right):
     # /dev/tty/serial{NUM} 먼저 확인하기
-    ser = serial.Serial('/dev/ttyUSB0',9600)
+    ser = serial.Serial('/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_5573631303835101F150-if00',9600)
 
     cmd = "L{:03d}R{:03d}\n".format(left, right).encode('ascii')
     print("Debug cmd -- %s" %cmd)
